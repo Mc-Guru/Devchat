5321f7f68de4e8346c4db703c5fa5d0950582f78
Mc-Guru <97976716+Mc-Guru@users.noreply.github.com>
2023-04-24 13:10:20 +0200

    E

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-mkdir my-chat-app
-cd my-chat-app
-npm init
-npm install express socket.io --save
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
